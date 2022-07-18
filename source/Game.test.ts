7b2efe3ac11b07f7ce02f4ddcc1fa346c54d285e
Kent Rasmussen <hyprkookeez@gmail.com>
2022-07-18 17:42:10 +0900

    Change error test

--- source/Game.test.ts
+++ source/Game.test.ts
@@ -4,19 +4,19 @@ test("Game.login", async () => {
     try {
         await Game.login("hyprkookeez@gmail.com", "thisIsNotMyRealPasswordLOL")
     } catch (e) {
-        expect(e).toMatch("Wrong Password")
+        expect(e.message).toMatch("Wrong Password")
     }
 
     try {
         await Game.loginJSONFile("this.file.does.not.exist")
     } catch (e) {
-        expect(e).toMatch(/Could not locate '.+?'\./)
+        expect(e.message).toMatch(/Could not locate '.+?'\./)
     }
 
     try {
         await Game.loginJSONFile("credentials.json.sample")
     } catch (e) {
-        expect(e).toMatch("Wrong Password")
+        expect(e.message).toMatch("Wrong Password")
     }
 
     expect(await Game.loginJSONFile("credentials.json")).toBe(true)
