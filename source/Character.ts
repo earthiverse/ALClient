81aa95cb6ecfa58e632349b8a45447fb57790683
Kent Rasmussen <hyprkookeez@gmail.com>
2022-10-05 20:01:31 +0900

    Attempt to fix updateLoop (again)

--- source/Character.ts
+++ source/Character.ts
@@ -139,13 +139,13 @@ export class Character extends Observer implements CharacterData {
     protected async updateLoop(): Promise<void> {
         if (this.noReconnect) return
         if (!this.socket || this.socket.disconnected || !this.ready) {
-            this.timeouts.set("updateLoop", setTimeout(this.updateLoop, Constants.UPDATE_POSITIONS_EVERY_MS))
+            this.timeouts.set("updateLoop", setTimeout(async () => { this.updateLoop() }, Constants.UPDATE_POSITIONS_EVERY_MS))
             return
         }
 
         if (this.lastPositionUpdate === undefined) {
             this.updatePositions()
-            this.timeouts.set("updateLoop", setTimeout(this.updateLoop, Constants.UPDATE_POSITIONS_EVERY_MS))
+            this.timeouts.set("updateLoop", setTimeout(async () => { this.updateLoop() }, Constants.UPDATE_POSITIONS_EVERY_MS))
             return
         }
 
@@ -157,11 +157,11 @@ export class Character extends Observer implements CharacterData {
         if (msSinceLastUpdate > Constants.UPDATE_POSITIONS_EVERY_MS) {
             // Update now
             this.updatePositions()
-            this.timeouts.set("updateLoop", setTimeout(this.updateLoop, Constants.UPDATE_POSITIONS_EVERY_MS))
+            this.timeouts.set("updateLoop", setTimeout(async () => { this.updateLoop() }, Constants.UPDATE_POSITIONS_EVERY_MS))
             return
         } else {
             // Update in a bit
-            this.timeouts.set("updateLoop", setTimeout(this.updateLoop, Constants.UPDATE_POSITIONS_EVERY_MS - msSinceLastUpdate))
+            this.timeouts.set("updateLoop", setTimeout(async () => { this.updateLoop() }, Constants.UPDATE_POSITIONS_EVERY_MS - msSinceLastUpdate))
             return
         }
     }
