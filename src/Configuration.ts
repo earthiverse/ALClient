e1cf048238fe07c4150f4cf145f6def424d7336c
Kent Rasmussen <hyprkookeez@gmail.com>
2024-09-16 14:30:15 +0900

    reduce socket timeout

--- src/Configuration.ts
+++ src/Configuration.ts
@@ -4,7 +4,7 @@ export class Configuration {
   /** How long to wait for the `welcome` socket when initially connecting */
   public CONNECT_TIMEOUT_MS = 5000;
   /** How long to wait for a response for a socket emit */
-  public SOCKET_EMIT_TIMEOUT_MS = 2500;
+  public SOCKET_EMIT_TIMEOUT_MS = 1000;
   /** How long to wait before reconnecting (https://socket.io/docs/v4/client-api/#managerreconnectiondelayvalue) */
   public SOCKET_RECONNECT_DELAY_MS = 5000;
   /** Randomization factor for reconnecting (https://socket.io/docs/v4/client-options/#randomizationfactor) */
