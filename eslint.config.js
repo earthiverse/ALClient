2c27462ced5cbf29d295f023f7b44a1496b4920d
Kent Rasmussen <hyprkookeez@gmail.com>
2026-08-30 09:45:14 +0900

    update eslint to ignore loss-of-precision in definitions

--- eslint.config.js
+++ eslint.config.js
@@ -1,13 +1,26 @@
 // @ts-check
 
 import eslint from "@eslint/js"
+import { defineConfig } from "eslint/config"
 import eslintConfigPrettier from "eslint-config-prettier"
 import tseslint from "typescript-eslint"
 
 // TODO: Fix problems when `tseslint.configs.strict` is added
-export default tseslint.config(eslint.configs.recommended, ...tseslint.configs.recommended, eslintConfigPrettier, {
-    ignores: ["build/*"],
-    rules: {
-        "@typescript-eslint/consistent-type-imports": "error",
+export default defineConfig(
+    eslint.configs.recommended,
+    ...tseslint.configs.recommended,
+    eslintConfigPrettier,
+    {
+        ignores: ["build/*"],
+        rules: {
+            "@typescript-eslint/consistent-type-imports": "error",
+        },
     },
-})
+    {
+        files: ["source/definitions/**/*.test.ts"],
+        rules: {
+            "no-loss-of-precision": "off",
+            "@typescript-eslint/no-loss-of-precision": "off",
+        },
+    },
+)
